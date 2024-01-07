ba8db8d8ed2fe490e8864603c277baf575b550af
Ihar Voitau <igorgrime@gmail.com>
2024-01-07 18:17:09 +0300

    feat: solve rotateMatrix task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -373,8 +373,6 @@ function getSpiralMatrix(size) {
   return matrix;
 }
 
-console.log(getSpiralMatrix(3));
-
 /**
  * Rotates a matrix by 90 degrees clockwise in place.
  * Take into account that the matrix size can be very large. Consider how you can optimize your solution.
@@ -390,8 +388,17 @@ console.log(getSpiralMatrix(3));
  *    [7, 8, 9]         [9, 6, 3]
  *  ]                 ]
  */
-function rotateMatrix(/* matrix */) {
-  throw new Error('Not implemented');
+function rotateMatrix(matrix) {
+  const startMatrix = JSON.parse(JSON.stringify(matrix));
+  const resultMatrix = matrix;
+
+  for (let i = 0; i < resultMatrix.length; i += 1) {
+    for (let j = 0; j < resultMatrix[i].length; j += 1) {
+      resultMatrix[j][resultMatrix.length - i - 1] = startMatrix[i][j];
+    }
+  }
+
+  return matrix;
 }
 
 /**
